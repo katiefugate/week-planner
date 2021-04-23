0d06066b88d0752cb78ba3c91a8bef1c24d7ef3a
katiefugate <katiiierochelle@yahoo.com>
2021-04-23 11:58:06 -0700

    in progress.

--- main.js
+++ main.js
@@ -16,12 +16,27 @@ function store(event) {
 
 window.addEventListener('beforeunload', store);
 
+function contentLoadHandler(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    for(var k = 0; k < $viewDays.length; k++) {
+      if (data.entries[i].day === $viewDays[k].dataset.view) {
+        $viewDays[k].appendChild(addEntry(data.entries[i]));
+      }
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', contentLoadHandler);
+
 var $addButton = document.querySelector('.add-button');
 var $submitButton = document.querySelector('.submit-button');
 var $modalContainer = document.querySelector('.modal-container');
 var $overlay = document.querySelector('.overlay');
 var $form = document.querySelector('form');
 var $viewDays = document.querySelectorAll('tbody');
+var $dayTitle = document.querySelector('h2');
+var $updateModal = document.querySelector('.update-modal-container');
+var $table = document.querySelector('table')
 
 function addHandler(event) {
   $modalContainer.className = 'modal-container';
@@ -42,8 +57,11 @@ function submitHandler(event) {
   data.entries.unshift(entryObj);
   data.nextEntryId++;
   for(var i = 0; i < $viewDays.length; i++) {
-  if (entryObj.day === $viewDays[i].dataset.view) {
-    $viewDays[i].appendChild(addEntry(entryObj));
+    if (entryObj.day === $viewDays[i].dataset.view) {
+      console.log($viewDays[i].dataset.view);
+      console.log(entryObj.day)
+      $viewDays[i].appendChild(addEntry(entryObj));
+    }
   }
 }
 
@@ -61,21 +79,32 @@ function addEntry(entry) {
   tRow.appendChild(description);
   time.textContent = entry.time;
   description.textContent = entry.description;
-
+  return tRow;
 }
 
 function daysHandler(event) {
   for (var i = 0; i < $eachDay.length; i++) {
     if (event.target.getAttribute('id') === $eachDay[i].getAttribute('id')) {
       $viewDays[i].className = 'day';
-      for(var i = 0; i <data.entries.length; i++) {
-
-      }
+      $dayTitle.textContent = 'Scheduled events for ' + $eachDay[i].textContent;
     } else {
       $viewDays[i].className = 'day hidden';
     }
-    console.log($viewDays[i].className);
   }
 }
 
 $allDays.addEventListener('click', daysHandler);
+
+var $updateForm = document.querySelector('.update-form');
+console.log($updateForm)
+
+function updateHandler(event) {
+  if(event.target.className === 'update-button') {
+    $updateModal.className = 'update-modal-container';
+    $overlay.className = 'overlay';
+  }
+  $updateForm.elements.description.value = event.target.parentNode.firstChild.textContent;
+  console.log(event.target.parentNode)
+}
+
+$table.addEventListener('click', updateHandler);
